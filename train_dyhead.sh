7d95aa0334641f25c73d9af9c04d44432506a0aa
jakebiros <jakebiros@gmail.com>
2021-12-12 15:45:17 +0900

    Remove unnecessary true input

--- train_dyhead.sh
+++ train_dyhead.sh
@@ -1,1 +1,1 @@
-poetry run yolo-train --model config/yolov3-tiny-voc.cfg --data config/voc.data --n_cpu 4 --evaluation_interval 15 --use_dyhead True
+poetry run yolo-train --model config/yolov3-tiny-voc.cfg --data config/voc.data --n_cpu 4 --evaluation_interval 15 --use_dyhead
